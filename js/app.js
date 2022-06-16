4b33bed96da3251467b457a5028219039edc047f
Lucas Leonetti <lucasleone95@gmail.com>
2022-06-16 14:11:35 -0300

    v1.1.0-stable

--- js/app.js
+++ js/app.js
@@ -1,12 +1,20 @@
 ; (function () {
     'use strict'
 
-    var juego = {
-        palabra: 'ALURA',
-        estado: 7,
-        adivinado: ['A', 'L'],
-        errado: ['B', 'J', 'K', 'C']
-    }
+    var palabras = [
+        'ALURA',
+        'NIÑO',
+        'AFINIDAD',
+        'PROGRAMAR',
+        'ORACLE',
+        'YOUTUBE'
+    ]
+
+    // variable para almacenar la configuracion actual
+    var juego = null
+    // para ver si ya se ha enviado alguna alerta
+    var finalizado = false
+
 
     var $html = {
         hombre: document.getElementById('hombre'),
@@ -15,103 +23,137 @@
     }
 
     function dibujar(juego) {
-        //actualizar la imagen del hombre
-        let $elem;
-        $elem = $html.hombre;
-        let estado = juego.estado;
-        if (estado == 8) {
-            estado == juego.previo;
+        // Actualizar la imagen del hombre
+        var $elem
+        $elem = $html.hombre
+
+        var estado = juego.estado
+        if (estado === 8) {
+            estado = juego.previo
         }
-        $elem.src = './img/estados/0' + estado + '.png';
+        $elem.src = './img/estados/0' + estado + '.png'
 
-        //creamos las letras a adivinadas
-        let palabra = juego.palabra;
-        let adivinado = juego.adivinado;
-        $elem = $html.adivinado;
-        //borramos los elementos anteriores
-        $elem.innerHTML = '';
-
+        // Creamos las letras adivinadas
+        var palabra = juego.palabra
+        var adivinado = juego.adivinado
+        $elem = $html.adivinado
+        // borramos los elementos anteriores
+        $elem.innerHTML = ''
         for (let letra of palabra) {
-            let $span = document.createElement('span');
-            let $txt = document.createTextNode('');
-
-            if (adivinado.indexOf(letra) >= 0) {
-                $txt.nodeValue = letra;
+            let $span = document.createElement('span')
+            let $txt = document.createTextNode('')
+            if (adivinado.has(letra)) {
+                $txt.nodeValue = letra
             }
-
-            $span.setAttribute('class', 'letra adivinada');
-            $span.appendChild($txt);
-            $elem.appendChild($span);
+            $span.setAttribute('class', 'letra adivinada')
+            $span.appendChild($txt)
+            $elem.appendChild($span)
         }
 
-        //creamos las letras erradas
-        let errado = juego.errado;
-        $elem = $html.errado;
-        //borramos los elementos anteiores
-        $elem.innerHTML = '';
+        // Creamos las letras erradas
+        var errado = juego.errado
+        $elem = $html.errado
+        // Borramos los elementos anteriores
+        $elem.innerHTML = ''
         for (let letra of errado) {
-            let $span = document.createElement('span');
-            let $txt = document.createTextNode(letra);
-            $span.setAttribute('class', 'letra errada');
-            $span.appendChild($txt);
-            $elem.appendChild($span);
+            let $span = document.createElement('span')
+            let $txt = document.createTextNode(letra)
+            $span.setAttribute('class', 'letra errada')
+            $span.appendChild($txt)
+            $elem.appendChild($span)
         }
-
     }
 
     function adivinar(juego, letra) {
-        let estado = juego.estado;
-        //si ya persite o ganaste no hay que hacer nada!
-        if (estado == 1 || estado == 8) {
-            return;
+        var estado = juego.estado
+        // Si ya se ha perdido, o ganado, no hay que hacer nada
+        if (estado === 1 || estado === 8) {
+            return
         }
 
-        //si ya has adivinado o errado la letra tampoco hay que hacer nada!
-        let adivinado = juego.adivinado;
-        let errado = juego.errado;
-
-        if (adivinado.indexOf(letra) >= 0 || errado.indexOf(letra) >= 0) {
-            return;
+        var adivinado = juego.adivinado
+        var errado = juego.errado
+        // Si ya hemos adivinado o errado la letra, no hay que hacer nada
+        if (adivinado.has(letra) || errado.has(letra)) {
+            return
         }
 
-        let palabra = juego.palabra;
-        //si es letra la palabra
-        if (palabra.indexOf(letra) >= 0) {
-            let ganado = true;
-            //debemos ver si llegamos al estado ganado
-            for (let l of palabra) {
-                if (adivinado.indexOf(l) < 0 && l != letra) {
-                    ganado = false;
-                    juego.previo = juego.estado;
-                    break;
-                }
+        var palabra = juego.palabra
+        var letras = juego.letras
+        // Si es letra de la palbra
+        if (letras.has(letra)) {
+            // agregamos a la lista de letras adivinadas
+            adivinado.add(letra)
+            // actualizamos las letras restantes
+            juego.restante--
+
+            // Si ya se ha ganado, debemos indicarlo
+            if (juego.restante === 0) {
+                juego.previo = juego.estado
+                juego.estado = 8
             }
-            //si ya hermos ganado, debemos indicarlo
-            if (ganado) {
-                juego.ganado = 8;
-            }
-            //agregamos la letra a la lista de letras adivinadas
-            adivinado.push(letra);
         } else {
-            //si no es letra, acercamos al hombre a la ahorca
-            juego.estado--;
-            //agregamos la letra, a la lista de letras erradas
-            errado.push(letra);
+            // Si no es letra de la palabra, acercamos al hombre un paso más de su ahorca
+            juego.estado--
+            // Agregamos la letra, a la lista de letras erradas
+            errado.add(letra)
         }
-
     }
 
-
     window.onkeypress = function adivinarLetra(e) {
-        let letra = e.key;
-        letra = letra.toUpperCase();
+        var letra = e.key
+        letra = letra.toUpperCase()
         if (/[^A-ZÑ]/.test(letra)) {
             return
         }
-        adivinar(juego, letra);
-        dibujar(juego);
+        adivinar(juego, letra)
+        var estado = juego.estado
+        if (estado === 8 && !finalizado) {
+            setTimeout(alertaGanado, 0)
+            finalizado = true
+        } else if (estado === 1 && !finalizado) {
+            let palabra = juego.palabra
+            let fn = alertaPerdido.bind(undefined, palabra)
+            setTimeout(fn, 0)
+            finalizado = true
+        }
+        dibujar(juego)
     }
 
-    dibujar(juego)
+    window.nuevoJuego = function nuevoJuego() {
+        var palabra = palabraAleatoria()
+        juego = {}
+        juego.palabra = palabra
+        juego.estado = 7
+        juego.adivinado = new Set()
+        juego.errado = new Set()
+        finalizado = false
+
+        var letras = new Set()
+        for (let letra of palabra) {
+            letras.add(letra)
+        }
+        juego.letras = letras
+        juego.restante = letras.size
+
+        dibujar(juego)
+        console.log(juego)
+    }
+
+
+    function palabraAleatoria() {
+        var index = ~~(Math.random() * palabras.length)
+        return palabras[index]
+    }
+
+    function alertaGanado() {
+        alert('Felicidades, ganaste!')
+    }
+
+    function alertaPerdido(palabra) {
+        alert('Lo siento, perdiste... la palabra era: ' + palabra)
+    }
+
+    nuevoJuego()
 
 }())
